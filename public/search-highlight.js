9e2b54fe0d7c21ce7573176cd8e2d302cecd3fa0
Michał Budzyński <michal@virtualdesign.pl>
2014-09-23 01:01:00 +0100

    [SEARCH-HIGHLIGHT] - :sunglasses: working lib

--- public/search-highlight.js
+++ public/search-highlight.js
@@ -1,13 +1,10 @@
 var SH = function(node, word) {
-  node = Array.prototype.slice.call(node.children);
-  node.forEach(function(node) {
-    var content = node.innerHTML;
-    content = content.replace(new RegExp(word, 'gi'), '<span class="sh-search">' + word + '</span>');
-    node.innerHTML = content;
-  });
+  setTimeout(function() {
+    node = Array.prototype.slice.call(node.children);
+    node.forEach(function(node) {
+      var content = node.innerHTML;
+      content = content.replace(new RegExp(word, 'gi'), '<span class="sh-search">' + word + '</span>');
+      node.innerHTML = content;
+    });
+  }, 500);
 };
-
-
-setTimeout(function() {
-  SH(document.getElementsByTagName('pre')[0], 'first');
-}, 500);
