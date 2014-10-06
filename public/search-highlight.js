2f8f4020dbf494f06095963818efdc18008faf53
Michał Budzyński <michal@virtualdesign.pl>
2014-10-06 01:01:00 +0100

    [SEARCH-HIGHLIGHT] - :dizzy_face: back out to prev ver

--- public/search-highlight.js
+++ public/search-highlight.js
@@ -4,13 +4,11 @@ var SH = function(node, phrase) {
   setTimeout(function() {
     phrase.forEach(function(word) {
       console.log('elo', word);
-      mainNode = Array.prototype.slice.call(node.children);
+      var mainNode = Array.prototype.slice.call(node.children);
       mainNode.forEach(function(node) {
-        var content = node.textContent;
-        if (content.indexOf(word)) {
-          content = content.replace(new RegExp(word, 'gi'), '<span class="sh-search">' + word + '</span>');
-          node.innerHTML = content;
-        }
+        var content = node.innerHTML;
+        content = content.replace(new RegExp(word, 'gi'), '<span class="sh-search">' + word + '</span>');
+        node.innerHTML = content;
       });
     });
   }, 500);
