199addc7b16d4e048747aca1ecaadb127110ef37
Michał Budzyński <michal@virtualdesign.pl>
2014-10-06 01:01:00 +0100

    [SEARCH-HIGHLIGHT] - :pear: work on multiple words in phrase

--- public/search-highlight.js
+++ public/search-highlight.js
@@ -2,11 +2,14 @@ var SH = function(node, phrase) {
   phrase = phrase.replace(/([^A-Za-z0-9]+)/g, '');
   phrase = phrase.split(' ');
   setTimeout(function() {
-    mainNode = Array.prototype.slice.call(node.children);
-    mainNode.forEach(function(node) {
-      var content = node.innerHTML;
-      content = content.replace(new RegExp(word, 'gi'), '<span class="sh-search">' + word + '</span>');
-      node.innerHTML = content;
+    phrase.forEach(function(word) {
+      console.log('elo', word);
+      mainNode = Array.prototype.slice.call(node.children);
+      mainNode.forEach(function(node) {
+        var content = node.innerHTML;
+        content = content.replace(new RegExp(word, 'gi'), '<span class="sh-search">' + word + '</span>');
+        node.innerHTML = content;
+      });
     });
   }, 500);
 };
