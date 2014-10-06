d589ab346eaa80503cc15725606a23263b33cf29
Michał Budzyński <michal@virtualdesign.pl>
2014-10-06 01:01:00 +0100

    [SEARCH-HIGHLIGHT] - :seat: change special chars to spaces

--- public/search-highlight.js
+++ public/search-highlight.js
@@ -1,7 +1,8 @@
 var SH = function(node, word) {
+  word = word.replace(/([^A-Za-z0-9])/g, '');
   setTimeout(function() {
-    node = Array.prototype.slice.call(node.children);
-    node.forEach(function(node) {
+    mainNode = Array.prototype.slice.call(node.children);
+    mainNode.forEach(function(node) {
       var content = node.innerHTML;
       content = content.replace(new RegExp(word, 'gi'), '<span class="sh-search">' + word + '</span>');
       node.innerHTML = content;
